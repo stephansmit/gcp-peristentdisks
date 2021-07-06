53c0b689cc6029bbb71b12907a26fb661b59b38f
Stephan Smit <stephansmit@hotmail.com>
2021-07-06 20:22:49 +0200

    added

--- deploy.sh
+++ deploy.sh
@@ -41,28 +41,28 @@ rsync -Pav -e "ssh -i $KEY_NAME" $DATA_DIR stephansmit_hotmail_com@$HOST:/mnt/di
 HOST=$(gcloud compute instances describe $VM_NAME --zone=$COMPUTE_ZONE --format='get(networkInterfaces[0].accessConfigs[0].natIP)')
 ssh -o StrictHostKeyChecking=no -i $KEY_NAME stephansmit_hotmail_com@$HOST 'bash -s' < unmount_disk.sh
 #sleep 10s
-##create snapshot
-#gcloud compute disks snapshot $DISK_NAME --snapshot-names=$SNAPSHOT_NAME --storage-location=$STORAGE_LOCATION --zone=$COMPUTE_ZONE --quiet
-##detach the disk
-#gcloud compute instances detach-disk $VM_NAME --disk=$DISK_NAME --zone=$COMPUTE_ZONE --quiet
-##delete the disk
-#gcloud compute instances delete $VM_NAME --zone=$COMPUTE_ZONE --quiet
-##delete the instance
-#gcloud compute disks delete $DISK_NAME --zone=$COMPUTE_ZONE --quiet
-#
-#
-#
-#
-#rm ~/.ssh/known_hosts
-##create disk from snapshot
-#gcloud compute disks create $DISK_NAME --zone=$COMPUTE_ZONE --type=$DISK_TYPE --size=$DISK_SIZE --source-snapshot $SNAPSHOT_NAME --quiet
-##create instance
-#gcloud compute instances create $VM_NAME --zone=$COMPUTE_ZONE --image=$IMAGE_NAME --image-project=$IMAGE_PROJECT --metadata enable-oslogin=TRUE --quiet
-##attach disk
-#gcloud compute instances attach-disk $VM_NAME --disk=$DISK_NAME --zone=$COMPUTE_ZONE --device-name=$DEVICE_NAME --quiet
-##mount disk with snapshot
-#echo "waiting a few secs"
-#sleep 30s
-#HOST=$(gcloud compute instances describe $VM_NAME --zone=$COMPUTE_ZONE --format='get(networkInterfaces[0].accessConfigs[0].natIP)')
-#ssh  -o StrictHostKeyChecking=no -i $KEY_NAME stephansmit_hotmail_com@$HOST 'bash -s' < mount_disk_snapshot.sh
-#
+#create snapshot
+gcloud compute disks snapshot $DISK_NAME --snapshot-names=$SNAPSHOT_NAME --storage-location=$STORAGE_LOCATION --zone=$COMPUTE_ZONE --quiet
+#detach the disk
+gcloud compute instances detach-disk $VM_NAME --disk=$DISK_NAME --zone=$COMPUTE_ZONE --quiet
+#delete the disk
+gcloud compute instances delete $VM_NAME --zone=$COMPUTE_ZONE --quiet
+#delete the instance
+gcloud compute disks delete $DISK_NAME --zone=$COMPUTE_ZONE --quiet
+
+
+
+
+rm ~/.ssh/known_hosts
+#create disk from snapshot
+gcloud compute disks create $DISK_NAME --zone=$COMPUTE_ZONE --type=$DISK_TYPE --size=$DISK_SIZE --source-snapshot $SNAPSHOT_NAME --quiet
+#create instance
+gcloud compute instances create $VM_NAME --zone=$COMPUTE_ZONE --image=$IMAGE_NAME --image-project=$IMAGE_PROJECT --metadata enable-oslogin=TRUE --quiet
+#attach disk
+gcloud compute instances attach-disk $VM_NAME --disk=$DISK_NAME --zone=$COMPUTE_ZONE --device-name=$DEVICE_NAME --quiet
+#mount disk with snapshot
+echo "waiting a few secs"
+sleep 30s
+HOST=$(gcloud compute instances describe $VM_NAME --zone=$COMPUTE_ZONE --format='get(networkInterfaces[0].accessConfigs[0].natIP)')
+ssh  -o StrictHostKeyChecking=no -i $KEY_NAME stephansmit_hotmail_com@$HOST 'bash -s' < mount_disk_snapshot.sh
+
